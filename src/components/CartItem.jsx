7211af616b42205ee2e47bab1fb688941abd097b
Shams-Joha <shamsjoha3@gmail.com>
2024-11-06 02:52:40 +0600

    finished wishlist

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,6 +1,6 @@
 import { RiDeleteBin5Line } from "react-icons/ri";
 
-const CartItem = ({ product }) => {
+const CartItem = ({ product, showButton }) => {
     const { product_title, description, price, product_image } = product;
     return (
         <div className="rounded-2xl p-10 grid grid-cols-6 items-center">
@@ -8,13 +8,16 @@ const CartItem = ({ product }) => {
                 <img className="w-44 h-36 rounded-2xl" src={product_image}></img>
             </div>
 
-            <div className="flex-col space-y-5 col-span-4">
+            <div className="flex-col space-y-4 col-span-4">
                 <h1 className="text-2xl font-bold">{product_title}</h1>
                 <p className='text-gray-600'>{description}</p>
                 <p className="font-bold text-gray-600 text-lg">{`Price: $${price}`}</p>
+                {
+                    showButton && (<button className="btn rounded-full text-white bg-violet-500 font-bold py-2">Add to Cart</button>)
+                }
             </div>
             <div className="col-span-1">
-                <button><RiDeleteBin5Line size={30} color="red"/></button>
+                <button><RiDeleteBin5Line size={30} color="red" /></button>
             </div>
 
         </div>);
--- src/pages/WishLayout.jsx
+++ src/pages/WishLayout.jsx
@@ -1,9 +1,20 @@
+import CartItem from "../components/CartItem";
+import { getPrevList } from "../utils";
 
 
 const WishLayout = () => {
+    const data = getPrevList();
     return (
-        <div>
-            <h2>You are in the Wishlist</h2>
+        <div className="container my-10 mx-auto">
+            <div className="flex justify-start items-center mb-5">
+                <div><h2 className="font-bold text-lg">WishList</h2></div>
+            </div>
+            {/* Cart Products */}
+            <div>
+                {
+                    data.map(item => <CartItem product={item} key={item.product_id} showButton={true}></CartItem>)
+                }
+            </div>
         </div>
     );
 };
